e004c33092ddc79d5a3ade9dcc71512836f56262
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-05 23:31:37 -0600

    Add files via upload

---
+++ gotas.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Número de gotas
+n_drops = 10
+drop_speed = 0.02  # Velocidad de caída
+
+# Crear un array para las posiciones iniciales de las gotas (fuera de la pantalla)
+drop_positions = np.random.rand(n_drops) * 2 - 1  # Posiciones horizontales
+drop_heights = np.random.rand(n_drops) * -1  # Alturas iniciales (fuera del cuadro)
+
+# Crear la figura y los ejes
+fig, ax = plt.subplots(figsize=(5, 8))
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+
+# Crear los círculos que representarán las gotas
+drops, = ax.plot([], [], 'o', color='blue', markersize=8)
+
+# Función de inicialización
+def init():
+    drops.set_data([], [])
+    return drops,
+
+# Función de actualización de la animación
+def update(frame):
+    global drop_heights
+
+    # Actualizar las alturas de las gotas
+    drop_heights += drop_speed  # Las gotas caen hacia abajo
+    
+    # Reposicionar las gotas que caen fuera de la pantalla (reseteo)
+    drop_heights[drop_heights > 1] = np.random.rand() * -1
+    
+    # Actualizar los datos de las gotas
+    drops.set_data(drop_positions, drop_heights)
+    return drops,
+
+# Crear la animación
+ani = FuncAnimation(fig, update, frames=np.arange(0, 200), init_func=init, blit=True, interval=50)
+
+plt.show()
